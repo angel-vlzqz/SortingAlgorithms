5a346bcdb362e502b43fccd93240c8ddf7628f58
angel-vlzqz <106009767+angel-vlzqz@users.noreply.github.com>
2024-06-14 20:38:53 -0700

    initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+from src.bubblesort import bubbleSort
+from src.heapsort import heapSort
+from src.mergesort import mergeSort
+from src.quicksort import quicksort
+from src.radixsort import radixSort
+from src.selectionsort import selectionSort
+
+if __name__ == "__main__":
+    print("test")
